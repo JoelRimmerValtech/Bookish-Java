95a258d4b009d4b41439ffe14d2456e962404c89
dali546 <35352237+dali546@users.noreply.github.com>
2019-03-26 16:29:14 +0000

    push

---
+++ src/main/java/org/softwire/training/bookish/controllers/StaffBooksController.java
@@ -0,0 +1,56 @@
+package org.softwire.training.bookish.controllers;
+
+import org.softwire.training.bookish.models.database.Book;
+import org.softwire.training.bookish.models.database.Member;
+import org.softwire.training.bookish.models.page.BookModel;
+import org.softwire.training.bookish.models.page.StaffPageMemberModel;
+import org.softwire.training.bookish.services.BookService;
+import org.softwire.training.bookish.services.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.view.RedirectView;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/staff/books")
+public class StaffBooksController {
+
+    private final BookService bookService;
+
+    @Autowired
+    public StaffBooksController(BookService bookService) {
+        this.bookService = new BookService();
+    }
+
+    @RequestMapping("")
+    ModelAndView books() {
+        List<Book> bookList = bookService.getBooks("");
+        BookModel bookModel = new BookModel();
+        bookModel.setBookList(bookList);
+        return new ModelAndView("staffBooks", "books", bookModel);
+    }
+
+    @RequestMapping("/add")
+    RedirectView addBooks(@ModelAttribute Book book) {
+        bookService.addBook(book);
+        return new RedirectView("/staff/books");
+    }
+
+    @RequestMapping("/edit")
+    RedirectView editBooks(@RequestBody Book book) {
+        bookService.editBook(book);
+        return new RedirectView("/staff/books");
+    }
+
+    @RequestMapping("/delete")
+    RedirectView deleteBooks(@RequestBody Book book) {
+        bookService.deleteBook(book.getBookId());
+        return new RedirectView("/staff/books");
+    }
+
+}
